cd0b6d52c12df11d7b2bbd0ddaebc8d8ab570da5
Arrtiom <iamnotsssure@seznam.cz>
2023-04-19 16:57:51 +0200

    js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log("Hi NPM")
+
+let a = 10;
+let b = 20; 
+let sum = a + b;
+console.log("Souceet: ", sum);
+
+for (let i = 0; i < 10; i++) {
+    console.log("Cyklus poradi: ", i);
+}
+
+PROCVICOVANI JS 
+SOLR
